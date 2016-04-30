d7c3ef2440e64d69d50002c4a5ab6cc53e7523a7
shinjukujohnny <kikkawa1323@fout.jp>
2016-04-30 22:16:36 +0900

    add initial files

---
+++ SomeReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SomeReader
+//
+//  Created by Hisafumi Kikkawa on 2016/04/30.
+//  Copyright © 2016年 shinjukujohnny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
